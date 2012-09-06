05a4a385643fb8aab368adbb6bf097bb68926d74
Code Ass <aycabta@gmail.com>
2012-09-06 21:16:05 +0900

    added description of replace src of img tag with absolute path

--- plugins/file_binder.rb
+++ plugins/file_binder.rb
@@ -9,9 +9,14 @@
 # If you wrote a entry in "source/_posts/YYYY-DD-MM-title-of-a-entiry.markdown",
 # you can attach the files that are given the name of
 # "source/_posts/YYYY-DD-MM-title-of-a-entiry_filename-of-image.png" for example.
-# The attached file puts out into the same directory of the entry by the name of "filename-of-image.png",
+# The attached files puts out into the same directory of the entry by the name of "filename-of-image.png",
 # in this case it is "public/blog/YYYY/DD/MM/title-of-a-entry/filename-of-image.png".
-# You can refer the file from the entry by img or others tags.
+# You can refer the files from the entry by img or others tags.
+#
+# Replace "./" that is head of src in a img tag with config['url'] + "/blog/YYYY/DD/MM/title-of-a-entry/".
+# config['url'] is written in _config.yml with "url: ".
+# So src is published absolute path without problems
+# if you write {% img ./filename-of-image.png %}.
 #
 # Support customized permalink in _config.yml that is different from "/blog/:year/:month/:day/:title/".
 #
